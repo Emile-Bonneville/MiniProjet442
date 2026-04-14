311ed67745dbfd73e5da0505414ab57e0212de03
Emile-Bonneville <emile.bonneville@ens-paris-saclay.fr>
2026-04-14 17:51:20 +0200

    Initial commit

---
+++ Core/Inc/menu.h
@@ -0,0 +1,18 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "stdint.h"
+
+typedef enum
+{
+  MENU_ACTION_AUCUNE = 0,
+  MENU_ACTION_LANCER_DAMES_LOCAL,
+  MENU_ACTION_LANCER_DAMES_UART_BLANC,
+  MENU_ACTION_LANCER_DAMES_UART_NOIR
+} MenuAction;
+
+void Menu_Reinitialiser(void);
+void Menu_Afficher(void);
+MenuAction Menu_GererTouch(uint16_t x, uint16_t y);
+
+#endif /* MENU_H */
